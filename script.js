1a4c08647c884ec768c0730f8d95a65950230bcb
MagicCogs <thegingericing@gmail.com>
2024-04-10 20:27:19 +0100

    réécriture de "document"

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let form = document.querySelector('form');
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
-    let firstName = doucment.querySelector('#first-name').value;
+    let firstName = document.querySelector('#first-name').value;
     let lastName = document.querySelector('#last-name').value;
     let message = document.querySelector('#message').value;
     let errormessage = document.querySelector('#error-message');
