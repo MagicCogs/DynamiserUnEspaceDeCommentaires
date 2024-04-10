3ff52f8ae431f33a3ceb20c2e6919d04fd98353f
MagicCogs <thegingericing@gmail.com>
2024-04-10 13:25:38 +0100

    création de la variable de la valeur du prénom

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ let form = document.querySelector('form');
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
+    let firstName = doucment.querySelector('#first-name').value;
 })
