5bcb7d50a94eb3ebcb4cfcc06ef4f7bfc07e231a
MagicCogs <thegingericing@gmail.com>
2024-04-10 13:29:03 +0100

    création de la variable du message d'erreur

--- script.js
+++ script.js
@@ -5,4 +5,5 @@ form.addEventListener('submit', function(event) {
     let firstName = doucment.querySelector('#first-name').value;
     let lastName = document.querySelector('#last-name').value;
     let message = document.querySelector('#message').value;
+    let errormessage = document.querySelector('#error-message');
 })
