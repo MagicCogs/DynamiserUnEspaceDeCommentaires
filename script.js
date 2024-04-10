eb56adf7613ea369b0ed9c814d34299ee0da8f7f
MagicCogs <thegingericing@gmail.com>
2024-04-10 20:28:48 +0100

    retiré le nom de famille par défaut à côté de celui que l'utilisateur a entré

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ form.addEventListener('submit', function(event) {
         newComment.classList.add("flex", "space-x-4", "text-sm", "text-gray-500");
         newComment.innerHTML = `
         <div class="flex-1 py-10 border-t border-gray-200">
-                  <h3 class="font-medium text-gray-900">${firstName} ${lastName} Gibbons</h3>
+                  <h3 class="font-medium text-gray-900">${firstName} ${lastName}</h3>
                   <div class="prose prose-sm mt-4 max-w-none text-gray-500">
                     <p>${message}</p>
                   </div>
