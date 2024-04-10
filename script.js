e813fe79e199576f5914d2c28682063d886afd86
MagicCogs <thegingericing@gmail.com>
2024-04-10 13:26:43 +0100

    création de la variable de la valeur du nom de famille

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ let form = document.querySelector('form');
 form.addEventListener('submit', function(event) {
     event.preventDefault();
     let firstName = doucment.querySelector('#first-name').value;
+    let lastName = document.querySelector('#last-name').value;
 })
